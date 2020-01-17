a4c1fa798c3cc9bdf0305998380165c684f0230a
LAPTOP-IEO23PUP\javed <smile110@gmail.com>
2020-01-17 19:27:32 +0500

    Changes in the dashboard

--- src/app/pages/dashboard/complaints/complaints.page.ts
+++ src/app/pages/dashboard/complaints/complaints.page.ts
@@ -6,6 +6,80 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./complaints.page.scss'],
 })
 export class ComplaintsPage implements OnInit {
+  complaints = [
+    {
+      id: "ISO60120-6224952",
+      date: "06",
+      month: "Jan",
+      year: "2020",
+      title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+      status: "Closed",
+      statussign:"danger"
+    },
+    {
+      id: "ISO60120-6224952",
+      date: "22",
+      month: "Dec",
+      year: "2019",
+      title: "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+      status: "Open",
+      statussign:"tertiary"
+    },
+    {
+      id: "ISO63620-6224952",
+      date: "12",
+      month: "Aug",
+      year: "2019",
+      title: "Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+      status: "In Progress",
+      statussign:"tertiary"
+    },
+    {
+      id: "ISO60120-6224952",
+      date: "06",
+      month: "Jan",
+      year: "2019",
+      title: "Lorem amet adipiscing elit, sed do eiusmod tempor incididunt ut labore et",
+      status: "Open",
+      statussign:"success"
+    },
+    {
+      id: "ISO60120-6224952",
+      date: "06",
+      month: "Jan",
+      year: "2019",
+      title: "Tempor incididunt ut labore et dolore magna aliqua lorem ipsum dolor sit amet",
+      status: "Relief Granted",
+      statussign:"success"
+    },
+    {
+      id: "ISO60120-6224952",
+      date: "06",
+      month: "Jan",
+      year: "2019",
+      title: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi",
+      status: "Open",
+      statussign:"warning"
+    },
+    {
+      id: "ISO63620-6224952",
+      date: "12",
+      month: "Aug",
+      year: "2019",
+      title: "Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+      status: "In Progress",
+      statussign:"success"
+    },
+    {
+      id: "ISO60120-6224952",
+      date: "06",
+      month: "Jan",
+      year: "2019",
+      title: "Lorem amet adipiscing elit, sed do eiusmod tempor incididunt ut labore et",
+      status: "Open",
+      statussign:"warning"
+    }
+  ]
 
   constructor() { }
 
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+const API_URL = environment.apiUrl;
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { }
+  postData(url){
+
+  }
+}
+ 
